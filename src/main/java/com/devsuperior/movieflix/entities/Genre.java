a730e94dd62b24622f3be91275cc4df2546fa399
cunharodrigues <pwcunha@gmail.com>
2021-12-14 16:15:08 -0300

    Created Classes User, Role, Review, Movie, Genre

---
+++ src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -0,0 +1,92 @@
+package com.devsuperior.movieflix.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "tb_genre")
+public class Genre implements UserDetails, Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	
+	public Genre() {
+		
+	}
+
+	public Genre(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object getMovies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -0,0 +1,153 @@
+package com.devsuperior.movieflix.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "tb_movie")
+public class Movie implements UserDetails, Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String title;
+	private String subTitle;
+	private Integer year;
+	private String imgUri;
+	private String synopsis;
+	
+	public Movie() {
+		
+	}
+
+	public Movie(Long id, String title, String subTitle, Integer year, String imgUri, String synopsis) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.subTitle = subTitle;
+		this.year = year;
+		this.imgUri = imgUri;
+		this.synopsis = synopsis;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSubTitle() {
+		return subTitle;
+	}
+
+	public void setSubTitle(String subTitle) {
+		this.subTitle = subTitle;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getImgUri() {
+		return imgUri;
+	}
+
+	public void setImgUri(String imgUri) {
+		this.imgUri = imgUri;
+	}
+
+	public String getSynopsis() {
+		return synopsis;
+	}
+
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setImgUrl(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setGenre(Genre genre) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getImgUrl() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getGenre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getReviews() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
